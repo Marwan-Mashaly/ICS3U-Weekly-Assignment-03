869aea6d746fd37e5fef43f4d8270df75edcb00e
Ubuntu <ubuntu@ip-172-31-30-160.ec2.internal>
2019-10-14 18:27:07 +0000

    added letters check program

---
+++ letter.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+# Created by Marwan Mashaly
+# Created on September 2019
+# This program tells user if the letter is a constant or vowel
+
+
+def main():
+    # This function tells user if the letter is a constant or vowel
+
+    # input
+    letter = input("Enter a letter to check: ")
+    print("")
+
+    # process
+    if letter == 'a':
+        # output
+        print("it's a Vowel")
+    elif letter == 'b':
+        # output
+        print("it's a Consant")
+    elif letter == 'c':
+        # output
+        print("it's a constant ")
+    elif letter == 'd':
+        # output
+        print("it's a constant ")
+    elif letter == 'e':
+        # output
+        print("it's a vowel ")
+    elif letter == 'f':
+        # output
+        print("it's a constant ")
+    elif letter == 'g':
+        # output
+        print("it's a constant ")
+    elif letter == 'h':
+        # output
+        print("it's a constant ")
+    elif letter == 'i':
+        # output
+        print("it's a vowel ")
+    elif letter == 'j':
+        # output
+        print("it's a constant ")
+    elif letter == 'k':
+        # output
+        print("it's a constant ")
+    elif letter == 'l':
+        # output
+        print("it's a constant ")
+    elif letter == 'm':
+        # output
+        print("it's a constant ")
+    elif letter == 'n':
+        # output
+        print("it's a constant ")
+    elif letter == 'o':
+        # output
+        print("it's a vowel ")
+    elif letter == 'p':
+        # output
+        print("it's a constant ")
+    elif letter == 'q':
+        # output
+        print("it's a constant ")
+    elif letter == 'r':
+        # output
+        print("it's a constant ")
+    elif letter == 's':
+        # output
+        print("it's a constant ")
+    elif letter == 't':
+        # output
+        print("it's a constant ")
+    elif letter == 'u':
+        # output
+        print("it's a vowel ")
+    elif letter == 'v':
+        # output
+        print("it's a constant ")
+    elif letter == 'w':
+        # output
+        print("it's a constant ")
+    elif letter == 'x':
+        # output
+        print("it's a constant ")
+    elif letter == 'z':
+        # output
+        print("it's a constant")
+    elif letter == 'y':
+        # output
+        print(" it's sometimes a vowel sometimes a constant")
+    else:
+        print("invalid letter ")
+
+
+if __name__ == '__main__':
+    main()
